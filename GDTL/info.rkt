12a0af711d885137e08bcc3ab369588de2fd7b7b
Joey Eremondi <JoeyEremondi@users.noreply.github.com>
2019-05-16 15:50:15 -0700

    Get rid of versions

--- GDTL/info.rkt
+++ GDTL/info.rkt
@@ -1,3 +1,3 @@
 #lang info
-(define build-deps '(("base" #:version "a406bc74d583ac7a61f651e5331bcfd0ecc1f73b") ("redex" #:version "c46896732085530f46d2252c11d804305fda59d0") ("sweet-exp" #:version "a3c1ae74c2e75e8d6164a3a9d8eb34335a7ba4de")))
+(define build-deps '("base" "redex-lib" "sweet-exp-lib"))
 (define scribblings '(("GDTL-manual.scrbl" ())))
